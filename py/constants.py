713bc8cf12767073421994f5a575ad687eb02969
vspup <51629242+vspup@users.noreply.github.com>
2025-11-13 16:52:21 -0500

    Remove legacy compatibility wrappers

--- py/constants.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility wrapper for :mod:`py.core.constants`."""
-
-from .core.constants import cfg
-
-__all__ = ["cfg"]
--- py/electabuzz_client.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility wrapper for :mod:`py.core.electabuzz_client`."""
-
-from .core.electabuzz_client import *  # noqa: F401,F403
